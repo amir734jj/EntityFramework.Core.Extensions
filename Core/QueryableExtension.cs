63594e60f00a491360421d71d2db6c6493999437
Seyedamirhossein Hesamian <seyedhesamian@northwesternmutual.com>
2019-12-09 15:16:01 -0600

    started working

---
+++ Core/QueryableExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Core
+{
+    /// <summary>
+    /// IQueryable extensions
+    /// </summary>
+    public static class QueryableExtension
+    {
+        /// <summary>
+        /// Converts P-SQL (ADO.NET) string given an IQueryable&lt;TEntity&gt; and DbContext
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="dbCtx"></param>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        public static string ToSql<TEntity>(this IQueryable<TEntity> query, DbContext dbCtx)
+        {
+            var modelGenerator = dbCtx.GetService<IQueryModelGenerator>();
+            var queryModel = modelGenerator.ParseQuery(query.Expression);
+            var databaseDependencies = dbCtx.GetService<DatabaseDependencies>();
+            var queryCompilationContext = databaseDependencies.QueryCompilationContextFactory.Create(false);
+            var modelVisitor = (RelationalQueryModelVisitor) queryCompilationContext.CreateQueryModelVisitor();
+            modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
+            var sql = modelVisitor.Queries.FirstOrDefault()?.ToString();
+            return sql;
+        }
+    }
+}
